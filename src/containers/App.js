146f434bbce0ff8fe754c9ecb20aefbc7e22614a
Dipto Karmakar <diptokmk47@gmail.com>
2019-06-07 01:27:12 +0600

    Add user location
    - Add user location and updated on display

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      err: ""
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      pos => {
+        this.setState({ lat: pos.coords.latitude });
+      },
+      err => {
+        this.setState({ errMessage: err.message });
+      }
+    );
+  }
+
   render() {
-    return <div />;
+    if (this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else if (this.state.errMessage) {
+      return <div>Error: {this.state.errMessage}</div>;
+    }
+    return <div>Loading . . . . </div>;
   }
 }
 
