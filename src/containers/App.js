e109d75fa487d397e14b8b5b1cfc5d362826ee9b
Dipto Karmakar <diptokmk47@gmail.com>
2019-06-10 19:04:32 +0600

    Add season display feature

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
+import SeasonDisplay from "../components/SeasonDisplay";
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      err: ""
-    };
-
+  state = {
+    lat: null,
+    err: ""
+  };
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      pos => {
-        this.setState({ lat: pos.coords.latitude });
-      },
-      err => {
-        this.setState({ errMessage: err.message });
-      }
+      pos => this.setState({ lat: pos.coords.latitude }),
+      err => this.setState({ errMessage: err.message })
     );
   }
 
   render() {
     if (this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     } else if (this.state.errMessage) {
       return <div>Error: {this.state.errMessage}</div>;
     }
